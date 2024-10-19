a5fdd24c78fcba3206347324f89abab959537147
MonsurAbdullah <monsur759@gmail.com>
2024-10-20 01:41:19 +0600

    Initial commit

---
+++ scripts/mint.js
@@ -0,0 +1,27 @@
+// Import necessary modules from Hardhat and SwisstronikJS
+const hre = require("hardhat");
+const { SwisstronikPlugin } = require("@swisstronik/web3-plugin-swisstronik");
+hre.web3.registerPlugin(new SwisstronikPlugin(hre.network.config.url));
+async function main() {
+    const replace_contractAddress = "0x7Ce457583D6EAfd3d22b437cAd1a0F4053B48728";
+    const [from] = await hre.web3.eth.getAccounts();
+    const contractFactory = await hre.ethers.getContractFactory("TestNFT");
+    const ABI = JSON.parse(contractFactory.interface.formatJson());
+    const contract = new hre.web3.eth.Contract(ABI, replace_contractAddress);
+    const replace_functionArgs = "0x18963378D34b17923b5e3ee1581B6379eA87E3CB"; // Recipient address
+    console.log("Minting 1 token...");
+    try {
+        const transaction = await contract.methods.safeMint(replace_functionArgs).send({ from });
+        console.log("Transaction submitted! Transaction details:", transaction);
+        // Display success message with recipient address
+        console.log(`Transaction completed successfully! ✅ Non-Fungible Token minted to ${replace_functionArgs}`);
+        console.log("Transaction hash:", transaction.transactionHash);
+    } catch (error) {
+        console.error(`Transaction failed! Could not mint NFT.`);
+        console.error(error);
+    }
+}
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
